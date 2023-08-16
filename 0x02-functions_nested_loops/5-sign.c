0ea1b51a17ae3359aa59844be4f9b928537152bf
basmalaalii <basmalaalii175@gmail.com>
2023-08-16 03:22:21 -0700

    C - Functions, nested loops C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -8,20 +7,20 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
-{
+{ 
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-       	else if (n == 0)
+	else if (n == 0)
 	{	
 		_putchar(48);
 		return (0);
 	}
 	else   
 	{       
-	       	_putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
